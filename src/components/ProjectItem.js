2fe1de9d9e301b21f3c69cbe21029ec10a85b4a4
+collins <+collinskipngetich60@gmail.com>
2024-02-05 22:31:59 +0300

    completed

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,7 +6,13 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {<ul>
+        {technologies.map((tech, index) => (
+          <li key={index}>
+            <span>{tech}</span>
+          </li>
+        ))}
+      </ul>  /* render a <span> for each technology in the technologies array */}
       </div>
     </div>
   );
